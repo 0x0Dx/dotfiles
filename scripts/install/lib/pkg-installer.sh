3b3704ec2444046d8a5a6d90a55b30672079b6d2
0x0D <0xOD@proton.me>
2026-02-06 14:10:32 +0100

    refactor: rename ILLOGICAL_IMPULSE_VIRTUAL_ENV to DAIFUKU_VIRTUAL_ENV

--- scripts/install/lib/pkg-installer.sh
+++ scripts/install/lib/pkg-installer.sh
@@ -76,11 +76,11 @@ install-uv(){
 
 install-python-packages(){
   UV_NO_MODIFY_PATH=1
-  ILLOGICAL_IMPULSE_VIRTUAL_ENV=$XDG_STATE_HOME/quickshell/.venv
-  x mkdir -p $(eval echo $ILLOGICAL_IMPULSE_VIRTUAL_ENV)
+  DAIFUKU_VIRTUAL_ENV=$XDG_STATE_HOME/quickshell/.venv
+  x mkdir -p $(eval echo $DAIFUKU_VIRTUAL_ENV)
   # we need python 3.12 https://github.com/python-pillow/Pillow/issues/8089
-  x uv venv --prompt .venv $(eval echo $ILLOGICAL_IMPULSE_VIRTUAL_ENV) -p 3.12
-  x source $(eval echo $ILLOGICAL_IMPULSE_VIRTUAL_ENV)/bin/activate
+  x uv venv --prompt .venv $(eval echo $DAIFUKU_VIRTUAL_ENV) -p 3.12 --clear
+  x source $(eval echo $DAIFUKU_VIRTUAL_ENV)/bin/activate
   if [[ "$INSTALL_VIA_NIX" = true ]]; then
     x nix-shell ${REPO_ROOT}/scripts/install/uv/shell.nix --run "uv pip install -r ${REPO_ROOT}/scripts/install/uv/requirements.txt"
   else
